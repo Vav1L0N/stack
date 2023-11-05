a5ae7e0f178571ea98bfd03e15b6b2b95d64edf1
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2023-11-05 17:36:20 +0300

    Add files via upload

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,13 +1,37 @@
 #include "stack.h"
+#include "stack.cpp"
+
+
+/*int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+*/
 
 int main()
 {
-	
-	TDynamicStack<int> stack;
-	stack.Push(1);
-	stack.Push(2);
-	stack.Push(3);
 
-	cout << "Stack contents: " << stack << endl;
+
+	TDynamicStack<int> st(10);
+
+	for (int i = 0; i < 10; i++)
+		st.Push(i + 1);
+	cout << "after 10 x push(): " << endl;
+	cout << st << endl << endl;
+	
+	cout << "getSize():" << endl;
+	cout << st.getSize() << endl << endl;
+
+	st.Pop();
+	cout << "after 1 x pop(): " << endl;
+	cout << st << endl << endl;
+
+	
+
+	cout << "isEmpty():" << endl;
+	cout << st.IsEmpty() << endl << endl;
+
+	return 0;
 }
 
